6f6b33acdb88cbcaf4f9395f3f40ceaad1dbb151
Bhagya Raj Varadaraju <bhagyaraj.varadaraju@gmail.com>
2022-12-29 19:42:33 -0500

    Challenge 4 done

--- index.js
+++ index.js
@@ -74,20 +74,23 @@ addGamesToPage(GAMES_JSON);
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalBackers = GAMES_JSON.reduce((count, game) => count + game.backers , 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
+contributionsCard.innerHTML = totalBackers.toLocaleString('en-US');
 
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
+const totalPledged = GAMES_JSON.reduce((count, game) => count + game.pledged, 0);
 
 // set inner HTML using template literal
+raisedCard.innerHTML = '$' + totalPledged.toLocaleString('en-US');
 
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
-
+gamesCard.innerHTML = GAMES_JSON.length.toLocaleString('en-US');
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
